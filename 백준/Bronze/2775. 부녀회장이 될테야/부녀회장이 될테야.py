b05bfe1284250d8e3db2068ad17fcd6f8e187418
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-20 01:48:13 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,10 @@
+T = int(input())
+for t in range(T):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n+1)] # 0층 1부터 n호까지의 사람수 리스트
+    for j in range(k): # 층수만큼 반복
+        for a in range(1, n): # 1 ~ n-1 까지 인덱스반복
+            people[a] += people[a-1] # 층별 각 호 사람 수
+            # print(people)
+    print(people[-1]) # 마지막 인덱스 출력하면 k층 n호 거주민 수
