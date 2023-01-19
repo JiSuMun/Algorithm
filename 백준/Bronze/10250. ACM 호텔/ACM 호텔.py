2c1cd0f4fd5dd90cd81823e44be6a6de96849e09
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-20 01:32:47 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,9 +1,15 @@
+# YYXX: YY-층수(H), XX-번호(W)
+# XX 작은 방 선호
+# XX 같을 때 낮은 YY 선호
+# 101-201-...-601-102-...
+# 1(101)-2(201)-..-7(102)-8(202)-...
+
 import sys
 T = int(sys.stdin.readline())
 for t in range(T):
     H, W, N = map(int, input().split())
-    f = N % H
-    cnt = (N // H) + 1
+    f = N % H # 손님 번호가 H넘어가면 층 = 손님수를 층수로 나눈 나머지
+    cnt = (N // H) + 1 # 엘리베이터에서 떨어진 곳
     if f == 0:
         f = H
         cnt -= 1
