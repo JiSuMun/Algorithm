d2c6ac973ba5c9a8dcdb797f2723c0362e3fbea3
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-20 17:28:54 +0900

    [D3] Title: 문자열의 거울상, Time: 138 ms, Memory: 59,380 KB -BaekjoonHub

--- SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
+++ SWEA/D3/10804. 문자열의 거울상/문자열의 거울상.py
@@ -1,13 +1,28 @@
+#T = int(input())
+#s_dict = {
+#    'b': 'd',
+ #   'd': 'b',
+  #  'p': 'q',
+#    'q': 'p'
+#}
+#for t in range(1, T + 1):
+ #   s = input()[::-1]
+  #  st = ''
+ #   for i in range(len(s)):
+#        st += s_dict[s[i]]
+#    print(f'#{t} {st}')
 T = int(input())
-s_dict = {
-    'b': 'd',
-    'd': 'b',
-    'p': 'q',
-    'q': 'p'
-}
 for t in range(1, T + 1):
     s = input()[::-1]
     st = ''
     for i in range(len(s)):
-        st += s_dict[s[i]]
+        if s[i] == 'b':
+            st += 'd'
+        elif s[i] == 'd':
+            st += 'b'
+        elif s[i] == 'p':
+            st += 'q'
+        else:
+            st += 'p'
+
     print(f'#{t} {st}')
