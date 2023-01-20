bd65d9ecfa1a387a422450134e1b45a5f2766e19
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-20 17:25:50 +0900

    [D3] Title: 직사각형 길이 찾기, Time: 245 ms, Memory: 59,940 KB -BaekjoonHub

--- SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.py
+++ SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.py
@@ -1,7 +1,18 @@
+#T = int(input())
+#for t in range(1, T + 1):
+#    square = list(map(int, input().split()))
+#    for i in range(3):
+ #      if square.count(square[i]) % 2 == 1:
+ #           print(f'#{t} {square[i]}')
+#            break
 T = int(input())
-for t in range(1, T + 1):
-    square = list(map(int, input().split()))
-    for i in range(3):
-        if square.count(square[i]) % 2 == 1:
-            print(f'#{t} {square[i]}')
-            break
+for t in range(1, T+1):
+    a, b, c = input().split()
+    if a == b:
+        r = c
+    else:
+        if a == c:
+            r = b
+        else:
+            r = a
+    print(f'#{t} {r}')
