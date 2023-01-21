3d8e34c67b628cac14cd6a16a51b91939978e105
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-22 02:42:25 +0900

    [Silver V] Title: 소수, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,18 +1,18 @@
 M = int(input())
 N = int(input())
 li = []
-for i in range(M, N+1):
-    c = 0
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                c += 1
-                break
-        if c == 0:
-            li.append(i)
 
-if len(li) < 1:
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            break
+    else:
+        li.append(i)
+
+if(len(li) == 0):
     print(-1)
 else:
     print(sum(li))
-    print(min(li))
+    print(li[0])
