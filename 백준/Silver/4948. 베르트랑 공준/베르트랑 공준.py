1320c7ff7e4438ae120fc9a26f5b93ac01e6d425
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-24 02:31:11 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1128 ms, Memory: 41852 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,3 +1,4 @@
+# 1156ms
 from math import sqrt
 def sosu(n):
     if n == 1:
@@ -23,3 +24,27 @@ while True:
             cnt += 1
     print(cnt)
     n = int(input())
+
+# 1340ms
+# from math import sqrt
+# def Prime(n):
+#     if n == 1: return False
+#     for i in range(2, int(sqrt(n)+1)):
+#         if n % i == 0: return False
+#     return True
+
+# num_list = list(range(2, 246912))
+# prime = []
+
+# for i in num_list:
+#     if Prime(i):
+#         prime.append(i)
+
+# while True:
+#     n = int(input())
+#     cnt = 0
+#     if n == 0: break
+#     for j in prime:
+#         if n < j <= 2*n:
+#             cnt += 1
+#     print(cnt)
