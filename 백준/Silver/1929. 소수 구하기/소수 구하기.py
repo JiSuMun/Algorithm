ed1c7ebe49a0ad85bb9e8d46d715e1748e375d60
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-23 15:52:07 +0900

    [Silver III] Title: 소수 구하기, Time: 3140 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,14 @@
-import sys
-M, N = map(int, sys.stdin.readline().split())
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+
+M, N = map(int, input().split())
 
 for i in range(M, N+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5) + 1):
-        if i % j == 0:
-            break
-    else:
+    if isPrime(i):
         print(i)
