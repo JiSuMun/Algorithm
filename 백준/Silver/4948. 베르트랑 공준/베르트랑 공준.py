87ecdd4a953fdb92ca91aad002539005ad35cd50
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-24 03:12:40 +0900

    [Silver II] Title: 베르트랑 공준, Time: 276 ms, Memory: 34472 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,50 +1,22 @@
-# 1156ms
+import sys
 from math import sqrt
-def sosu(n):
-    if n == 1:
-        return False
-    for i in range(2, int(sqrt(n)+1)):
-        if n % i == 0:
-            return False
-    return True
+input = sys.stdin.readline
+N = 123456 * 2 + 1
+P = [True] * N
+for i in range(2, int(sqrt(N)+1)):
+    if P[i]:
+        for j in range(2*i, N, i):
+            P[j] = False
 
-num_list = list(range(2, 246912))
-prime = []
-
-for i in num_list:
-    if sosu(i):
-        prime.append(i)
-n = int(input())
-while True:
+def prime_cnt(n):
     cnt = 0
-    if n == 0:
-        break
-    for j in prime:
-        if n < j <= 2*n:
+    for i in range(n+1, n*2 + 1):
+        if P[i]:
             cnt += 1
     print(cnt)
+
+while True:
     n = int(input())
-
-# 1340ms
-# from math import sqrt
-# def Prime(n):
-#     if n == 1: return False
-#     for i in range(2, int(sqrt(n)+1)):
-#         if n % i == 0: return False
-#     return True
-
-# num_list = list(range(2, 246912))
-# prime = []
-
-# for i in num_list:
-#     if Prime(i):
-#         prime.append(i)
-
-# while True:
-#     n = int(input())
-#     cnt = 0
-#     if n == 0: break
-#     for j in prime:
-#         if n < j <= 2*n:
-#             cnt += 1
-#     print(cnt)
+    if n == 0:
+        break
+    prime_cnt(n)
