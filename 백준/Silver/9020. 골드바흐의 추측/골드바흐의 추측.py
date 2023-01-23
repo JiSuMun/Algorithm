46af4dce4f1efd7d52f3e668f60d05a6631da047
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-24 02:54:33 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 488 ms, Memory: 30620 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,18 +1,19 @@
-# n을 반으로 나누고 각자 +1, -1해보면서 두 수가 모두 소수인지 확인함
-from math import sqrt
-def Prime(n):
-    if n == 1: return False
-    for i in range(2, int(sqrt(n)+1)):
-        if n % i == 0: return False
-    return True
-
-for _ in range(1, int(input())+1):
-    n = int(input())
-    a, b = n//2, n//2
-    while a > 0:
-        if Prime(a) and Prime(b):
-            print(a, b)
-            break
-        else:
-            a -= 1
-            b += 1
+def Prime():
+    P = [False, False] + [True] * 10000
+    for i in range(2, 101):
+        if P[i] == True:
+            for j in range(i+i, 10001, i):
+                P[j] = False
+    
+    T = int(input())
+    for _ in range(T):
+        n = int(input())
+        a, b = n//2, n//2
+        for _ in range(10000):
+            if P[a] and P[b]:
+                print(a, b)
+                break
+            else:
+                a -= 1
+                b += 1
+Prime()
