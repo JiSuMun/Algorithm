c086dbf6b89c4a9432940a26c3a9c762d98aca57
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-25 01:15:48 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,17 +1,10 @@
 import sys
 input = sys.stdin.readline
-
-arr = []
-for _ in range(9):
-    arr.append(list(map(int, input().split())))
-a = 0
-b = 0
-m = -1
+m, a, b = -1, 0, 0
 for i in range(9):
+    li = list(map(int, input().split()))
     for j in range(9):
-        if arr[i][j] > m:
-            m = arr[i][j]
-            a = i + 1
-            b = j + 1
+        if m < li[j]:
+            m, a, b = li[j], i+1, j+1
 print(m)
 print(a, b)
