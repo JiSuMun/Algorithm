3fff2017cd184087610f8b267eca626383bac1cc
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 00:00:28 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+arr = [[0]*100 for _ in range(100)] # 전부 0을 가지는 2차원 배열
+P = int(input())
+for _ in range(P):
+    x, y = list(map(int, input().split()))
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1 # 0으로 도배된 101 *101 크기의 도화지에서 색종이 위치에 1표시
+result = 0
+for i in arr:
+    result += i.count(1) # 1 카운팅
+print(result)
