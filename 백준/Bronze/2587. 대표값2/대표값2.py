b2d3c46a5928d0983c1b241aca883a8423aad859
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 00:13:40 +0900

    [Bronze II] Title: 대표값2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,5 +1,4 @@
 import sys
 input = sys.stdin.readline
-nums = [int(input()) for _ in range(5)]
-s_nums = sorted(nums)
-print(sum(nums)//5, s_nums[2])
+nums = sorted([int(input()) for _ in range(5)])
+print(sum(nums)//5, nums[2])
