f51c2b7503f5ebbf47f3409442bafe4916f9cf42
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 02:38:34 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 460 ms, Memory: 222684 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,5 +1,13 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-li = sorted([int(input()) for _ in range(N)])
-print(*li, sep='\n')
+# python3 => 1340ms, pypy3 => 
+# import sys
+# input = sys.stdin.readline
+# N = int(input())
+# li = sorted([int(input()) for _ in range(N)])
+# print(*li, sep='\n')
+
+# pypy3 => 464ms
+from sys import stdin
+n = int(stdin.readline())
+
+nums = sorted(map(int, stdin.read().split())) # read(): 파일 전체의 내용을 하나의 문자열로 읽어옴
+print('\n'.join(map(str, nums))) # join은 리스트와 써야 하므로 map사용
