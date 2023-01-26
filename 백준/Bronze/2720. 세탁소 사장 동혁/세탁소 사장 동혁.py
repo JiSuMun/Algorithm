783ed3853a57f114fa695371e5bdfbcf64132d4f
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 13:43:05 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    C = int(input())
+    Q = C//25
+    C -= Q*25
+    D = C//10
+    C -= D*10
+    N = C//5
+    C -= N*5
+    P = C//1
+    print(Q, D, N, P)
