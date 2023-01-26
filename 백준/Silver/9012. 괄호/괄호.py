9f73c7f36bda6cc6661a4f62fadc771b1dafd29b
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 14:31:06 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -2,18 +2,10 @@ import sys
 input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
-    PS = list(input())
-    sum = 0
-
-    for i in PS:
-        if i =='(':
-            sum += 1
-        elif i == ')':
-            sum -= 1
-        if sum < 0:
-            print('NO')
-            break
-    if sum > 0:
-        print('NO')
-    elif sum == 0:
+    PS = input().rstrip()
+    while '()' in PS:
+        PS = PS.replace('()', '')       
+    if PS == '':
         print('YES')
+    else:
+        print('NO')
