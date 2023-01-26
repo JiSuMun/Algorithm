5e6afb65ef4a8f2293745c31f3342fde47f13506
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-26 11:47:20 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,3 +1,4 @@
+# 40ms
 import sys
 input = sys.stdin.readline
 N = int(input())
@@ -6,3 +7,16 @@ while len(nums) > 1:
     print(nums.pop(0), end=' ')
     nums.append(nums.pop(0))
 print(nums[0])
+
+# 44ms
+# def card():
+#     nums = list(range(1, int(input())+1))
+#     p_nums = []
+#     while nums:
+#         p_nums.append(nums.pop(0))
+#         if nums:
+#             nums.append(nums.pop(0))
+#     print(*p_nums)
+
+# if __name__ == '__main__':
+#     card()
