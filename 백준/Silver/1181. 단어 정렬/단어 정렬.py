0a2bb52e0f8bb58a1aba921366761df12bfb5c82
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-27 14:54:22 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 35168 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,8 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-li = []
-for _ in range(N):
-    li.append(input().rstrip())
-s_li = list(set(li))
-ss_li = []
-for i in s_li:
-    ss_li.append((len(i), i))
-for j, k in sorted(ss_li):
-    print(k)
+li = [input().rstrip() for _ in range(N)]
+li = list(set(li))
+li = sorted(li)
+li.sort(key = len)
+print(*li, sep='\n')
