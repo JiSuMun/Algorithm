701a3933524a869a025b7b3618ffe2137cf81856
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-27 16:16:38 +0900

    [Silver I] Title: 절댓값 힙, Time: 112 ms, Memory: 35568 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -2,13 +2,27 @@ import sys
 import heapq
 input = sys.stdin.readline
 N = int(input())
-abs_h = []
+p_heap = [] # 양수
+m_heap = [] # 음수
 for _ in range(N):
-    x= int(input())
-    if x != 0:
-        heapq.heappush(abs_h, (abs(x), x))
+    x = int(input())
+    if x:
+        if x > 0:
+            heapq.heappush(p_heap, x) # 양수에 추가
+        else:
+            heapq.heappush(m_heap, -x) # 음수에 추가
     else:
-        try:
-            print(heapq.heappop(abs_h)[1])
-        except:
-            print(0)
+        if p_heap:
+            if m_heap:
+                if p_heap[0] < abs(-m_heap[0]):
+                # if p_heap[0] < m_heap[0]:
+                    print(heapq.heappop(p_heap))
+                else:
+                    print(-heapq.heappop(m_heap))
+            else:
+                print(heapq.heappop(p_heap))
+        else:
+            if m_heap:
+                print(-heapq.heappop(m_heap))
+            else:
+                print(0)
