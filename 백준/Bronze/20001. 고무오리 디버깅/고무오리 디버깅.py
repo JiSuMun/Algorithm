573a5ce01faaa097f9b93eab2dde803f657275a2
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-27 14:05:40 +0900

    [Bronze II] Title: 고무오리 디버깅, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/20001. 고무오리 디버깅/고무오리 디버깅.py
+++ 백준/Bronze/20001. 고무오리 디버깅/고무오리 디버깅.py
@@ -1,19 +1,16 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-li = deque()
+cnt = 0
 while 1:
     s = input().rstrip()
-    if s == '고무오리 디버깅 시작': continue
     if s == '고무오리 디버깅 끝': break
-    if s == '문제': li.append(s)
-    elif s == '고무오리':
-        if len(li) == 0:
-            li.append('문제')
-            li.append('문제')
-        else:
-            li.pop()
-if li:
-    print('힝구')
-else:
+    else:
+        if s == '문제': cnt += 1
+        elif s == '고무오리':
+            if cnt <= 0:
+                cnt += 2
+            else: cnt -= 1
+if cnt == 0:
     print('고무오리야 사랑해')
+else:
+    print('힝구')
