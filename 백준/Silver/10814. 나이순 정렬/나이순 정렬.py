7439cd87df08ba99770719bc7621616fff10ec3b
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-29 04:18:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 100 ms, Memory: 38924 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,3 +1,17 @@
+# 264ms
+# import sys
+# input = sys.stdin.readline
+# N = int(input())
+# m_li = []
+# for _ in range(N):
+#     a, n = map(str, input().split())
+#     a = int(a)
+#     m_li.append((a, n))
+# m_li.sort(key = lambda x : x[0])
+# for i in m_li:
+#     print(i[0], i[1])
+
+# 112ms
 import sys
 m_li = sys.stdin.readlines()[1:]
 m_li.sort(key = lambda x: int(x.split()[0]))
