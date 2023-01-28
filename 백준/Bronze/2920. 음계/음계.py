aff339f1b8ee8bbdacc248bdbc83a82ad62c8e38
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-29 02:06:25 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,9 +1,6 @@
 from sys import stdin
 input = stdin.readline
 s_list = list(input().split())
-if s_list == sorted(s_list):
-    print('ascending')
-elif s_list == sorted(s_list, reverse = 1):
-    print('descending')
-else:
-    print('mixed')
+if s_list == sorted(s_list): print('ascending')
+elif s_list == sorted(s_list, reverse = 1): print('descending')
+else: print('mixed')
