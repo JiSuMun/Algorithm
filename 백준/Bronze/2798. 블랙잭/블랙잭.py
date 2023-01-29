bf1a28280e251294d75ae791128e932449551018
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-30 02:20:15 +0900

    [Bronze II] Title: 블랙잭, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,13 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-l = len(nums)
+nums = sorted(list(map(int, input().split())), reverse = 1)
 sum = 0
-for i in range(l-2):
-    for j in range(i+1, l-1):
-        for k in range(j+1, l):
-            if nums[i] + nums[j] + nums[k] > M:
-                continue
-            else:
-                sum = max(sum, nums[i] + nums[j] + nums[k])
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            cs = nums[i] + nums[j] + nums[k]
+            if cs <= M:
+                if sum < cs: sum = cs
+                break
 print(sum)
