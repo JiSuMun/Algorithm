370ccbd7df6d5a3ba11d205d6e44cf88f4e18b30
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-30 02:50:15 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,15 +1,12 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 N, K = map(int, input().split())
-li = deque()
+idx = 0
+li = [i for i in range(1, N+1)]
 res = []
-for i in range(1, N+1):
-    li.append(i)
 while li:
-    for j in range(K-1):
-        li.append(li.popleft())
-    res.append(li.popleft())
-print('<', end='')
-print(', '.join(map(str, res)), end='')
-print('>')
+    idx += K - 1
+    if idx >= len(li):
+        idx %= len(li)
+    res.append(str(li.pop(idx)))
+print("<", ", ".join(res), ">", sep="")
