a2330af652225ce463acbdec7b775f99cc8c2d24
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-01-31 13:48:46 +0900

    [Bronze II] Title: 대표값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.py
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -1,5 +1,5 @@
 import sys
-from collections import Counter
 input = sys.stdin.readline
 n_li = [int(input()) for _ in range(10)]
-print(sum(n_li)//10, Counter(n_li).most_common(1)[0][0], sep ='\n')
+cnt = [n_li.count(n) for n in n_li]
+print(sum(n_li)//10, n_li[cnt.index(max(cnt))], sep = '\n')
