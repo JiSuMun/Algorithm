01c398e810f3f5d150283356fe651008a1d3855b
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-01 14:47:19 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,12 +1,31 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-n = 666
+n = int(input())
 cnt = 0
-while 1:
-    if '666' in str(n):
+for i in range(9999):
+    if i%1000 == 666:
+        for j in range(1000):
+            ans = i*1000 + j
+            cnt += 1
+            if n == cnt:
+                break
+    elif i%100 == 66:
+        for j in range(100):
+            ans = i*1000 + 600 + j
+            cnt += 1
+            if n == cnt:
+                break
+    elif i%10 == 6:
+        for j in range(10):
+            ans = i*1000 + 660 + j
+            cnt += 1
+            if n == cnt:
+                break
+    else:
+        ans = i*1000 + 666
         cnt += 1
-    if cnt == N:
-        print(n)
+        if n == cnt:
+                break
+    if n == cnt:
         break
-    n += 1
+print(ans)
