4889eca04381788871dfa0f9161ee8412418fd43
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-02 02:17:06 +0900

    [Silver II] Title: 유기농 배추, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,40 +1,35 @@
 import sys 
 from collections import deque
 input = sys.stdin.readline
-
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 T = int(input())
-
-def BFS(graph, a, b):
+def BFS(board, a, b):
     queue = deque()
     queue.append((a,b))
-    graph[a][b] = 0
-
+    board[a][b] = 0 # 방문처리
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             if 0 <= nx < M and 0 <= ny < N:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
+                if board[nx][ny] == 1:
+                    board[nx][ny] = 0
                     queue.append((nx, ny))
     return
 
-for i in range(T):
+for _ in range(T):
     cnt = 0
     M, N, K = map(int,input().split())
-    graph = [[0]*N for _ in range(M)]
-
-    for j in range(K):
+    board = [[0]*N for _ in range(M)]
+    for i in range(K):
         x, y = map(int, input().split())
-        graph[x][y] = 1
-
+        board[x][y] = 1
+    
     for a in range(M):
         for b in range(N):
-            if graph[a][b] == 1:
-                BFS(graph, a, b)
+            if board[a][b] == 1:
+                BFS(board, a, b)
                 cnt += 1
     print(cnt)
