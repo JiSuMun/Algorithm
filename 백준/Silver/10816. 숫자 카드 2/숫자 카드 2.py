4335a1c64f61b5bcca9214efc8068a99511a1324
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-03 02:41:28 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 532 ms, Memory: 238884 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,3 +1,4 @@
+# 728ms, pypy3: 560ms
 import sys
 input = sys.stdin.readline
 N = int(input())
@@ -11,3 +12,21 @@ for i in n_li:
     else:
         nums[i] = 1
 print(' '.join(str(nums[i]) if i in nums else '0' for i in m_li))
+# 1180ms, pypy3: 728ms
+# import sys
+# input = sys.stdin.readline
+# N = int(input())
+# n_li = sorted(map(int, input().split()))
+# M = int(input())
+# m_li = list(map(int, input().split()))
+# idx, m_dict = 0, {}
+# for m in sorted(m_li): # m_li에 있는 것들만 알면 되고, n_li에 있는 모든 것들을 알 필요 없다.
+#     cnt = 0
+#     if m not in m_dict:
+#         while idx < N:
+#             if m == n_li[idx]:
+#                 cnt += 1; idx += 1
+#             elif m > n_li[idx]: idx += 1
+#             else: break
+#         m_dict[m] = cnt
+# print(' '.join(str(m_dict[m]) for m in m_li))
