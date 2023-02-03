c5f9318705ff83f68100682bca75a36eeb70741a
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-03 16:03:01 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,29 @@
-# 76ms
-import sys 
-from collections import deque
+import sys
 input = sys.stdin.readline
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+sys.setrecursionlimit(10000)
 T = int(input())
-def BFS(board, a, b):
-    queue = deque()
-    queue.append((a,b))
-    board[a][b] = 0 # 방문처리
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N:
-                if board[nx][ny] == 1:
-                    board[nx][ny] = 0
-                    queue.append((nx, ny))
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+def DFS(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if board[nx][ny] == 1:
+                board[nx][ny] = -1
+                DFS(nx, ny)
     return
 
 for _ in range(T):
-    cnt = 0
     M, N, K = map(int,input().split())
-    board = [[0]*N for _ in range(M)]
+    board = [[0]*M for _ in range(N)]
+    cnt = 0
     for i in range(K):
         x, y = map(int, input().split())
-        board[x][y] = 1
-    
-    for a in range(M):
-        for b in range(N):
-            if board[a][b] == 1:
-                BFS(board, a, b)
+        board[y][x] = 1
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] > 0:
+                DFS(i, j)
                 cnt += 1
     print(cnt)
