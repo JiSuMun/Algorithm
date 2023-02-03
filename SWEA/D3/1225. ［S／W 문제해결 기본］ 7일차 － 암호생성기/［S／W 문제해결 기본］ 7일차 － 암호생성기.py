280c67e43eccac1755c9ae87b75f14c60968ff60
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-03 16:29:12 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 110 ms, Memory: 48,404 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -0,0 +1,8 @@
+for _ in range(10):
+    t = int(input())
+    nums = list(map(int, input().split())) # 8개
+    while nums[-1] != 0:
+        for i in range(1, 6):
+            nums.append(nums.pop(0)-i)
+            if nums[-1] <= 0: nums[-1] = 0; break
+    print('#{} {} {} {} {} {} {} {} {}'.format(t, *nums))
