d755e395100bbb0397aec6e608a3ca20b41e41f1
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-06 03:29:39 +0900

    [Silver III] Title: 프린터 큐, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,22 +1,17 @@
 import sys
-from collections import deque
-input=sys.stdin.readline
+input = sys.stdin.readline
 T = int(input())
-for t in range(T):
+for _ in range(T):
     N, M = map(int, input().split())
-    P = deque(map(int, input().split())) # 중요도
-    queue = deque([0 for i in range(N)])# 위치 저장 리스트
-    queue[M] = 1
-    cnt = 0
-    while 1:
-        if P[0] == max(P): # 가장 큰 값이면 cnt에 1을 더해주고 삭제
-            cnt += 1
-            if queue[0] == 1: # 가장 큰 값이면서 내가 찾을 값이면 cnt출력하고 break
-                print(cnt)
-                break
-            else:
-                P.popleft()
-                queue.popleft()
-        else: # 가장 큰 값이 아니라면 맨 뒤에 넣어주고 삭제
-            P.append(P.popleft())
-            queue.append(queue.popleft())
+    li = list(map(int, input().split()))
+    que = [(i, li[i]) for i in range(N)]
+    li.sort(reverse = True)
+    res = 0
+    for el in li:
+        while que[0][1] != el:
+            tmp = que.pop(0)
+            que.append(tmp)
+        res += 1
+        if que.pop(0)[0] == M:
+            break
+    print(res)
