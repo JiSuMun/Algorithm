433e409bff5e0ed8e426c7f155a9b96dbfcd121c
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-06 15:27:14 +0900

    [Silver II] Title: 섬의 개수, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,25 +1,31 @@
 import sys
-sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 dx = [-1, 1, 0, 0, -1, 1, -1, 1]
 dy = [0, 0, -1, 1, -1, -1, 1, 1]
-def DFS(x, y):
-    graph[x][y] = 0
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
-            DFS(nx, ny)
-while 1:
+def BFS(i, j):
+    q = [[i, j]]
+    graph[i][j] = 0
+    while q:
+        y, x = q.pop(0)
+        for k in range(8):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < w and 0 <= ny < h and graph[ny][nx] == 1:
+                q.append([ny, nx])
+                graph[ny][nx] = 0
+
+while True:
     w, h = map(int, input().split())
-    if w == h == 0: break
+    if w == 0 and h == 0:
+        break
     graph = []
-    res = 0
     for _ in range(h):
         graph.append(list(map(int, input().split())))
-    for x in range(h):
-        for y in range(w):
-            if graph[x][y] == 1:
-                DFS(x, y)
-                res += 1
-    print(res)
+    
+    cnt = 0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                BFS(i, j)
+                cnt += 1
+    print(cnt)
