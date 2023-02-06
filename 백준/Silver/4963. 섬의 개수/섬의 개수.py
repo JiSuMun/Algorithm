f7092da6b0eb17a24c9975e605106195a75eb6ae
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-06 15:19:43 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 31588 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,22 +1,15 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 dx = [-1, 1, 0, 0, -1, 1, -1, 1]
 dy = [0, 0, -1, 1, -1, -1, 1, 1]
-# dx = [1, -1, 0, 0, 1, -1, 1, -1]
-# dy = [0, 0, -1, 1, -1, 1, 1, -1]
-def BFS(x, y):
+def DFS(x, y):
     graph[x][y] = 0
-    d = deque()
-    d.append([x, y])
-    while d:
-        a, b = d.popleft()
-        for i in range(8):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                d.append([nx, ny])
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+            DFS(nx, ny)
 while 1:
     w, h = map(int, input().split())
     if w == h == 0: break
@@ -27,6 +20,6 @@ while 1:
     for x in range(h):
         for y in range(w):
             if graph[x][y] == 1:
-                BFS(x, y)
+                DFS(x, y)
                 res += 1
     print(res)
