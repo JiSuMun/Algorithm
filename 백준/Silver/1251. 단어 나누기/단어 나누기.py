cb491fce612ec2b6d230def3a5d1c190fde49338
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-07 16:49:23 +0900

    [Silver V] Title: 단어 나누기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,12 +1,5 @@
 import sys
 input = sys.stdin.readline
-word = input().rstrip()
-n = len(word)
-ans = 'z' * n
-for i in range(n - 2):
-    t = word[:i + 1][::-1]
-    for j in range(i + 1, n - 1):
-        s = t + word[i + 1:j + 1][::-1] + word[j + 1:][::-1]
-        if s < ans:
-            ans = s
-print(ans)
+s = input().rstrip()[::-1]
+n = len(s)
+print(min(s[j:]+s[i:j]+s[:i] for i in range(1,n) for j in range(i+1,n)))
