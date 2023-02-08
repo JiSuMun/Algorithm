37b6e4300f4efa132d7a35cfd7e7eb1e55769505
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-08 17:45:11 +0900

    [Silver III] Title: 통계학, Time: 184 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,11 +1,37 @@
-import sys
-from collections import Counter
-input = sys.stdin.readline
-N = int(input())
-n_li = sorted([int(input()) for _ in range(N)])
-print(round(sum(n_li)/N))
-print(n_li[N//2])
-new = Counter(n_li).most_common()
-if len(new) > 1 and new[0][1] == new[1][1]: print(new[1][0])
-else: print(new[0][0])
-print(max(n_li)-min(n_li))
+from sys import stdin
+
+def sol2108():
+    n = int(stdin.readline()) # 숫자개수
+    # 입력받는 수는 -4000~4000까지 정수이므로 리스트만들어서 각각의 정수가 몇 개 포함되어 있는지 센다
+    counts = [0]*8001 
+    s = 0 # 합
+    for i in stdin:
+        num = int(i)
+        # 모든 수를 0이상으로 만들어 리스트에 저장
+        counts[num+4000] += 1
+
+    maxc = max(counts) # 가장 큰 수
+    mode = mcnt = 0 # 최빈값
+    idx = 0
+    med = None # 중앙값
+    mi, ma = 4001, -4001 
+    for i in range(8001):
+        cnt = counts[i]
+        if cnt == 0:
+            continue
+
+        num = i-4000 # 원래 수로 돌림
+        s += num*counts[i]
+        if cnt == maxc and mcnt < 2:
+            mode = num
+            mcnt += 1
+        mi = min(mi, num) # 최소값
+        ma = max(ma, num) # 최대값
+        idx += counts[i]
+        if idx >= n//2+1 and med == None:
+            med = num
+
+    print(round(s/n), med, mode, ma-mi, sep='\n')
+
+if __name__ == '__main__':
+    sol2108()
