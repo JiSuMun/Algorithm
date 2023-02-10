b873cd4133fb7a1d749aded76863aeae54a41642
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-11 02:17:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -7,8 +7,6 @@ start, end = 1, max(l) # 이분탐색 처음과 끝위치, 가장 짧은 길이 
 while start <= end: # 적절한 랜선의 길이를 찾는 알고리즘
     mid = (start + end) // 2 # 중간 위치
     line = 0 # 랜선 수
-    for i in l: line += i // mid # 분할 된 랜선 수
-    # 랜선의 개수가 분기점    
-    if line >= N: start = mid + 1
-    else: end = mid - 1
+    if sum([i//mid for i in l]) >= N: start = mid + 1
+    else: end = mid -1
 print(end)
