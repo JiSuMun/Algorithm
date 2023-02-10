361ffb14ed01be86deadaa1370a217ea52a4ff67
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-10 16:12:44 +0900

    [D3] Title: 반반, Time: 120 ms, Memory: 46,380 KB -BaekjoonHub

---
+++ SWEA/D3/11856. 반반/반반.py
@@ -0,0 +1,10 @@
+from collections import Counter
+T = int(input())
+res = 0
+for t in range(1, T+1):
+    S = input().rstrip()
+    s = Counter(S).most_common()
+    for i in s:
+        if len(i) == 2 and i[1] == 2: res = 'Yes'
+        else: res = 'No'
+    print(f'#{t} {res}')
