48d08d625d91b12731be59bc5f503ca8bec0e77c
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-10 23:26:03 +0900

    [Silver III] Title: N과 M (1), Time: 48 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,3 +1,19 @@
+# 52ms
 from itertools import permutations
 N,M = map(int, input().split(' '))
 print('\n'.join(list(map(' '.join, permutations(map(str, range(1, N+1)), M)))))
+# 164ms
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# li = [] # M개의 수열을 저장하기 위한 리스트
+# def s_15649():
+#     if len(li) == M: # M개가 되면 출력하고 함수 끝
+#         print(' '.join(map(str, li)))
+#         return
+#     for i in range(1, N+1): # 1~N
+#         if i not in li: # 리스트와 중복 확인
+#             li.append(i) 
+#             s_15649()
+#             li.pop() # 4 2 입력이라면 1 - 1,2 - 1 - 1,3 - 1 - 1,4
+# s_15649()
