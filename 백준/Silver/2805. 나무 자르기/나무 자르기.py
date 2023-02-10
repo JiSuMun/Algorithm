d710c873569fbb156706f81ab029a7fb6f138567
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-11 03:11:06 +0900

    [Silver II] Title: 나무 자르기, Time: 388 ms, Memory: 206152 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,20 +1,17 @@
 import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
-tree = list(map(int, input().split()))
+input = sys.stdin.read().splitlines()
+N,M = map(int,input[0].split())
+tree = [int(i) for i in input[1].split()]
 def sol(N, M , tree):
     ma = max(tree) - 1
     mi = 1
     mid = (ma + mi)//2
-    while ma >= mi:
+    while(ma >= mi):
         cnt = 0
         for i in tree:
-            if i - mid > 0:
-                cnt += i - mid
-        if cnt >= M:
-            mi = mid + 1
-        else:
-            ma = mid - 1
+            if(i - mid > 0): cnt += (i - mid)
+        if(cnt >= M): mi = mid + 1
+        else: ma = mid - 1
         mid = (ma + mi)//2
-    return print(ma)
-sol(N, M ,tree)
+    return ma
+print(sol(N, M ,tree))
