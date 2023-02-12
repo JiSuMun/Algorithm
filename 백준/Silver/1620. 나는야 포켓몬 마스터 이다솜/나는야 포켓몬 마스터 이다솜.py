3fdc228cd76f01c28053bba41bf755e58fa789bc
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-13 01:05:20 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 152 ms, Memory: 54904 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,3 +1,4 @@
+# 156ms
 import sys
 def sol():
     input = sys.stdin.readline
@@ -7,3 +8,29 @@ def sol():
     ans = [dd[int(t)] if (t:=input().strip()).isdigit() else str(d[t]) for i in range(M)]
     print("\n".join(ans))
 sol()
+# # python, pypy: 시간초과
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split()) # 도감에 수록되어 있는 포켓몬 개수, 맞춰야하는 문제의 개수
+# p_dict = {}
+# for i in range(1, N+1):
+#     p_dict[i] = input().rstrip()
+# m_li = [input().rstrip() for _ in range(M)]
+# for i in m_li:
+#     for k, v in p_dict.items():
+#         if i == str(k): print(v)
+#         if i == v: print(k)
+# # str.isdigit(): 문자열이 숫자로만 이루어져 있는지 확인하는 함수
+# # 244ms
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split()) # 도감에 수록되어 있는 포켓몬 개수, 맞춰야하는 문제의 개수
+# p_dict = {}
+# for i in range(1, N+1):
+#     a = input().rstrip()
+#     p_dict[i] = a
+#     p_dict[a] = i
+# for _ in range(M):
+#     pro = input().rstrip()
+#     if pro.isdigit(): print(p_dict[int(pro)])
+#     else: print(p_dict[pro])
