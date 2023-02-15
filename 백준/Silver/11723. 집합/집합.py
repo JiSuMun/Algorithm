01e6ed690475cbb78c878020dd2c2bb2f2bb67c0
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-15 16:28:34 +0900

    [Silver V] Title: 집합, Time: 5048 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+M = int(input())
+S = set()
+for i in range(M):
+    s = input().rstrip().split()
+    if len(s) == 1: # all, empty
+        if s[0] == 'all': S = set([i for i in range(1, 21)])
+        else: S = set()
+        continue
+    a, x = s[0], int(s[1])
+    if a == 'add': S.add(x)
+    elif a == 'remove': S.discard(x)
+    elif a == 'check': print(1 if x in S else 0)
+    elif a == 'toggle': S.discard(x) if x in S else S.add(x)
