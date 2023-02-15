e844947c3dafa5e3c00e05e0b18faa2bcb29a2dd
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-15 16:14:23 +0900

    [Silver III] Title: 파일 정리, Time: 156 ms, Memory: 53824 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,11 +1,10 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-n_dict = {}
-for _ in range(N):
-    f = input().rstrip().split('.')[1]
-    if f in n_dict: n_dict[f] += 1
-    else: n_dict[f] = 1
-file = sorted(n_dict.items(), key = lambda x:x[0])
-for i in file:
-    print(i[0], i[1])
+def file():
+    f = {}
+    N = int(input())
+    for _ in range(N):
+        ext = input().rstrip().split('.')[1]
+        f[ext] = f.get(ext, 0) + 1
+    print('\n'.join(i + ' ' + str(f[i]) for i in sorted(f.keys())))
+file()
