b5bb9a714602cd3dfc815ea6a111c86a945dfd25
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-16 17:27:50 +0900

    [Silver IV] Title: 차집합, Time: 576 ms, Memory: 253912 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,8 +1,20 @@
+# 620ms
 import sys
 input = sys.stdin.readline
 a, b = map(int, input().split())
 A = list(map(int, input().split()))
-B = set(map(int, input().split())) 
+B = set(map(int, input().split()))
 li = sorted([i for i in A if i not in B])
 print(len(li))
 if len(li) != 0: print(*li)
+# 912ms, pypy: 660ms
+# import sys
+# input = sys.stdin.readline
+# a, b = map(int, input().split())
+# A = set(map(int, input().split()))
+# B = set(map(int, input().split()))
+# li = []
+# for i in A:
+#     if i not in B: li.append(i)
+# print(len(li))
+# if len(li) != 0: print(*sorted(li))
