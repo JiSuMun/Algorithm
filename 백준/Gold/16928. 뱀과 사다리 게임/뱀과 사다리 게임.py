6a7f8670d93fdf6a3c480dc464116507b9bbf75b
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-19 03:09:28 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,27 +1,28 @@
 import sys
-from collections import deque
-input = sys.stdin.readline
-N, M = map(int, input().split())
-di = {}
-for _ in range(N+M):
-    a, b = map(int, input().split())
-    di[a] = b
 
-d = deque([1])
-board = [100]*101
-visited = [False]*101
-board[1] = 0
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+move_dic = {}
+for _ in range(n + m):
+  s, e = map(int, sys.stdin.readline().rstrip().split())
+  move_dic[s] = e
+
+q = []
+q.append(1)
+visited=[False]*101
+cnts = [100]*101
+cnts[1] = 0
 visited[1] = True
+while q:
+  v = q.pop(0)
+  for i in range(1, 7):
+    dest = v + i
+    if dest > 100: continue
+    if dest in move_dic:
+      dest = move_dic[dest]
+    if visited[dest]: continue
+    q.append(dest)
+    visited[dest] = True
+    cnts[dest] = cnts[v] + 1
 
-while d:
-    g = d.popleft()
-    for i in range(1, 7):
-        next = g + i
-        if next > 100: continue
-        if next in di:
-            next = di[next]
-        if visited[next]: continue # False이다
-        d.append(next)
-        visited[next] = True 
-        board[next] = board[g] + 1
-print(board[100])
+print(cnts[100])
