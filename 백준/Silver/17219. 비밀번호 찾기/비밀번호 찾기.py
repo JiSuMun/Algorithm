deaf3e20414a92dda661f56bacccc21099c5437d
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-19 02:02:20 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 176 ms, Memory: 52372 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,6 +1,30 @@
+# 180ms
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 d = dict(input().rstrip().split() for _ in range(N))
 res = [d[input().rstrip()] for _ in range(M)]
 print('\n'.join(res))
+# 시간초과
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# d = {}
+# for _ in range(N):
+#     a, p = input().rstrip().split()
+#     d[a] = p
+# for i in range(M):
+#     ad = input().rstrip()
+#     for k, v in d.items():
+#         if ad == k: print(v)
+# # 224ms
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# d = {}
+# for _ in range(N):
+#     a, p = input().rstrip().split()
+#     d[a] = p
+# for _ in range(M):
+#     k = input().rstrip()
+#     print(d[k])
