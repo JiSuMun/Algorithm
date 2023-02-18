3839e5a90792f708255b1bd7b86e454dd844c0b3
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-19 00:07:25 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,6 +2,6 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 li = sorted(list(map(int, input().split())))
-for i in range(1, N):
-    li[i] += li[i-1]
-print(sum(li))
+res, ans = 0, 0
+for i in li: res += i; ans += res
+print(ans)
