1382fb4af9cd491470fb6666af98be28b1113467
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-20 02:44:56 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,10 @@
 import sys
 input = sys.stdin.readline
-
-def fibo(N):   
-    zero = [1, 0, 1]
-    one = [0, 1, 1]
-    if len(zero) <= N:
-        for i in range(len(zero), N+1):
-            zero.append(zero[i-1]+zero[i-2])
-            one.append(one[i-1]+one[i-2])
-    print(zero[N], one[N])
-
 T = int(input())
-for t in range(T):
+d = [0] * 41 # 0 <= N <= 40
+d[0] = [1, 0]; d[1] = [0, 1]
+for i in range(2, 41):
+    d[i] = [d[i-1][1], sum(d[i-1])]
+for _ in range(T):
     N = int(input())
-    fibo(N)
+    print(*d[N])
