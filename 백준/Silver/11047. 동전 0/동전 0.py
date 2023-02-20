05d503bd478fe663053dabd4ec7aa6ff628c0997
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-20 23:43:05 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,6 +4,6 @@ N, K = map(int, input().split())
 A = [int(input()) for _ in range(N)]
 cnt = 0
 for i in range(N-1, -1, -1):
-    cnt += K // A[i]
-    K %= A[i]
+    if K // A[i] > 0: cnt += K // A[i]; K %= A[i]
+    else: continue
 print(cnt)
