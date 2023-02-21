12f5b20532b249ee66fb7ac3e3bec4c70d660227
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-21 22:35:54 +0900

    [Silver IV] Title: 듣보잡, Time: 68 ms, Memory: 45232 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,11 +1,7 @@
 import sys
-input = sys.stdin.readline
 N, M  = map(int, input().split())
-a = set(input().rstrip() for _ in range(N)) # 듣도 못한 사람
-b = set(input().rstrip() for _ in range(M)) # 보도 못한 사람
-res = []
-cnt = 0
-for i in a:
-    if i in b: res.append(i); cnt += 1
-    else: continue
-print(cnt, *sorted(a & b), sep='\n')
+arr = sys.stdin.read().splitlines()
+a = set(arr[:N])
+b = set(arr[N:])
+res = list(a & b)
+print(len(res), *sorted(res))
