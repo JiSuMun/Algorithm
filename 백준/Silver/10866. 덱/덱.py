b029e77fbd9137cf627c0584a24c219a6cfd2c06
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-21 22:55:55 +0900

    [Silver IV] Title: 덱, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,16 +1,15 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 N = int(input())
-d = deque()
+d = []
 for _ in range(N):
-    order = input().split()
+    order = input().rstrip().split()
     o = order[0]
-    if o == 'push_front': d.appendleft(order[1])
+    if o == 'push_front': d.insert(0, order[1])
     elif o == 'push_back': d.append(order[1])
-    elif o == 'pop_front': print(d.popleft()) if d else print(-1)
-    elif o == 'pop_back': print(d.pop()) if d else print(-1)      
+    elif o == 'pop_front': print(d.pop(0) if d else -1)
+    elif o == 'pop_back': print(d.pop() if d else -1)    
     elif o == 'size': print(len(d))
-    elif o == 'empty': print(1) if len(d) == 0 else print(0)       
-    elif o == 'front': print(d[0]) if d else print(-1)
-    elif o == 'back': print(d[-1]) if d else print(-1)
+    elif o == 'empty': print(0 if d else 1)       
+    elif o == 'front': print(d[0] if d else -1)
+    elif o == 'back': print(d[-1] if d else -1)
