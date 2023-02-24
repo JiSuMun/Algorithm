7c05fb9748e3bbe30b5a0ab164af8394af316bec
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-25 01:07:18 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,11 +1,28 @@
+# def solution(progresses, speeds):
+#     res = []
+#     while progresses:
+#         cnt = 0
+#         while progresses and progresses[0] >= 100:
+#             cnt += 1
+#             progresses.pop(0)
+#             speeds.pop(0)
+#         progresses = [progresses[i] + speeds[i] for i in range(len(progresses))]
+#         if cnt: res.append(cnt)        
+#     return res
+import math
+from collections import deque
 def solution(progresses, speeds):
     res = []
-    while progresses:
-        cnt = 0
-        while progresses and progresses[0] >= 100:
+    p = deque([math.ceil((100-a)/b) for a, b in zip(progresses, speeds)])
+    cnt = 1
+    a = p.popleft()
+    while p:
+        if p[0] <= a:
+            p.popleft()
             cnt += 1
-            progresses.pop(0)
-            speeds.pop(0)
-        progresses = [progresses[i] + speeds[i] for i in range(len(progresses))]
-        if cnt: res.append(cnt)        
+        else:
+            a = p.popleft()
+            res.append(cnt)
+            cnt = 1
+    res.append(cnt)        
     return res
