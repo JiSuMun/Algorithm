f0b0e36a1ae5bcee016071107467b9ed4ba10175
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-25 22:01:45 +0900

    [level 2] Title: 주식가격, Time: 13.21 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,10 +1,22 @@
+# def solution(prices):
+#     res = [0] * len(prices)
+#     for i in range(len(prices)):
+#         for j in range(i+1, len(prices)):
+#             if prices[i] <= prices[j]:
+#                 res[i] += 1
+#             else:
+#                 res[i] += 1
+#                 break
+#     return res
 def solution(prices):
     res = [0] * len(prices)
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                res[i] += 1
-            else:
-                res[i] += 1
-                break
+    stack = []
+    for i, price in enumerate(prices):
+        while stack and price < prices[stack[-1]]:
+            a = stack.pop()
+            res[a] = i - a # 결과_가격이 떨어지지 않은 시간 저장
+        stack.append(i) # 증가하는 시간을 stack에 저장
+    while stack:
+        a = stack.pop()
+        res[a] = len(prices) - a - 1
     return res
