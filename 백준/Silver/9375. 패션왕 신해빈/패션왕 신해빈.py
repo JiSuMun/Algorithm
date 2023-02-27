c8f1b6bd3ffe0d35a2352deab19df5cb85249a3a
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-02-27 21:17:04 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,13 +1,31 @@
+# 44ms
 import sys
 input = sys.stdin.readline
 T = int(input())
 for t in range(T):
     n = int(input())
-    c = {}
-    clothes = [input().rstrip().split() for _ in range(n)]
-    for k, v in clothes:
-        c[v] = c.get(v, 0) + 1
+    li = []
+    for _ in range(n): li.append(input().rstrip().split())
+    a = set()
+    c = []
+    for i in range(n):
+        a.add(li[i][1])
+        c.append(li[i][1])
     res = 1
-    for i in c:
-        res *= (c[i] + 1)
+    for j in a:
+        res *= c.count(j) + 1
     print(res-1)
+# 44ms
+# import sys
+# input = sys.stdin.readline
+# T = int(input())
+# for t in range(T):
+#     n = int(input())
+#     c = {}
+#     clothes = [input().rstrip().split() for _ in range(n)]
+#     for k, v in clothes:
+#         c[v] = c.get(v, 0) + 1
+#     res = 1
+#     for i in c:
+#         res *= (c[i] + 1)
+#     print(res-1)
