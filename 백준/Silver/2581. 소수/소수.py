f0b85d1c93469f60a853a248c76e774802f8b275
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-03-09 19:29:23 +0900

    [Silver V] Title: 소수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,3 +1,4 @@
+# 44ms
 import sys
 input = sys.stdin.readline
 M = int(input())
@@ -13,3 +14,43 @@ if sum(li) == 0: print(-1)
 else:    
     print(sum(li))
     print(min(li))
+# 56ms
+# import sys
+# input = sys.stdin.readline
+# M = int(input())
+# N = int(input())
+# li = []
+
+# for i in range(M, N+1):
+#     if i == 1:
+#         continue
+#     for j in range(2, int(i**0.5) + 1):
+#         if i % j == 0:
+#             break
+#     else:
+#         li.append(i)
+
+# if(len(li) == 0):
+#     print(-1)
+# else:
+#     print(sum(li))
+#     print(li[0])
+# 544ms
+# M = int(input())
+# N = int(input())
+# li = []
+# for i in range(M, N+1):
+#     c = 0
+#     if i > 1:
+#         for j in range(2, i):
+#             if i % j == 0:
+#                 c += 1
+#                 break
+#         if c == 0:
+#             li.append(i)
+
+# if len(li) < 1:
+#     print(-1)
+# else:
+#     print(sum(li))
+#     print(min(li))
