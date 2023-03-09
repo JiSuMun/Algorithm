82f663d0c57d456a00842e5e04aaf732a4953281
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-03-09 19:28:30 +0900

    [Silver V] Title: 소수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -2,19 +2,14 @@ import sys
 input = sys.stdin.readline
 M = int(input())
 N = int(input())
-li = []
-
-for i in range(M, N+1):
-    if i == 1:
-        continue
-    for j in range(2, int(i**0.5) + 1):
-        if i % j == 0:
-            break
-    else:
-        li.append(i)
-
-if(len(li) == 0):
-    print(-1)
-else:
+li = [1]*(N+1)
+li[1] = 0
+for i in range(2, int(N**0.5)+1):
+    if li[i]:
+        for j in range(i**2, N+1, i):
+            li[j] = 0
+li = [i for i in range(M, N+1) if li[i] == 1]
+if sum(li) == 0: print(-1)
+else:    
     print(sum(li))
-    print(li[0])
+    print(min(li))
