7a8f0e0280693fc3e0f5fe2c647c21302975db5f
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-03-24 03:17:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 212 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,37 @@
+# 11724
+# pypy: 
 import sys
 input = sys.stdin.readline
-from collections import deque
-sys.setrecursionlimit(10000)
-
-def dfs(start, depth):
-    visited[start] = True
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i, depth+1)
 
 N, M = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+seqnum = [0] * (N+1) # visited
+seq = 1
+ans = N # 반복하면서 노드의 개수를 줄여야 할 때 사용하는 변수, N 값이 변하지 않게 따로 변수 선언
 
 for _ in range(M):
+    if ans == 1:
+        input()
+        continue
+    valid = True
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    # 연결점 1 표시 (True 표시와 같은 의미)
+    if seqnum[u] == 0 and seqnum[v] == 0:
+        seqnum[u] = seqnum[v] = seq
+        seq += 1
+    elif seqnum[u] == 0:
+        seqnum[u] = seqnum[v]
+    elif seqnum[v] == 0:
+        seqnum[v] = seqnum[u]
+    else:
+        if seqnum[u] == seqnum[v]:
+            valid = False
+        else:
+            low_s = min(seqnum[u], seqnum[v])
+            high_s = max(seqnum[u], seqnum[v])
+            for i in range(1, N+1):
+                if seqnum[i] == high_s:
+                    seqnum[i] = low_s
+    if valid:
+        ans -= 1
 
-# 방문처리 위한 리스트
-visited = [False] * (N + 1)
-cnt = 0
-
-for i in range(1, N+1):
-    if not visited[i]:
-        if not graph[i]:
-            cnt += 1
-            visited[i] = True # 방문처리
-        else: # 연결 된 점이 있다면
-            dfs(i, 0)
-            cnt += 1
-print(cnt)
+print(ans)
