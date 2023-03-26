5b0749ec031a720f27cb42617d9743853326f50c
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-03-26 20:11:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,36 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-
 N, M, V = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    graph[a].sort()
-    graph[b].sort()
 
-def dfs(graph, V):   
+def dfs(V):
     visited[V] = 1
-    print(V, end=' ')
-    for i in graph[V]:
+    dfs_li.append(V)
+    for i in sorted(graph[V]):
         if not visited[i]:
-            dfs(graph, i)
+            dfs(i)
 
-def bfs(graph, V):   
-    d = deque([V])
+def bfs(V):
     visited[V] = 1
+    d = deque([V])
     while d:
         a = d.popleft()
-        print(a, end=' ')
-        for i in graph[a]:
-            if not visited[i]:
-                d.append(i)
-                visited[i] = 1
+        bfs_li.append(a)
+        for j in sorted(graph[a]):
+            if not visited[j]:
+                visited[j] = 1
+                d.append(j)
+
+dfs_li = []
+bfs_li = []
 visited = [0] * (N+1)
-dfs(graph, V)
-print('')
+dfs(V)
 visited = [0] * (N+1)
-bfs(graph, V)
+bfs(V)
+print(*dfs_li)
+print(*bfs_li)
