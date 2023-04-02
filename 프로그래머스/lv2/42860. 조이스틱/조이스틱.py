5e488e26817ec4a1e5f0d0f204928c11f8aa2415
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-04-02 15:14:05 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,13 @@
+def solution(name):
+    answer = 0
+    min_move = len(name) - 1
+    for i, alpha in enumerate(name):
+        up = ord(alpha) - ord('A')
+        down = ord('Z') - ord(alpha) + 1
+        answer += min(up, down)
+        next = i+1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        min_move = min(min_move, 2*i+len(name)-next, i+2*(len(name)-next))
+    answer += min_move
+    return answer
