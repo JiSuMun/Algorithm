6d0dc173ed7479f608dd642cd2159face3161877
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-04-09 02:47:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,22 +1,22 @@
-import heapq
+from heapq import heappop, heappush
 
 def solution(n, costs):
     graph = [[] for _ in range(n)]
     for island1, island2, cost in costs:
         graph[island1].append((island2, cost))
         graph[island2].append((island1, cost))
-
     visited = [False] * n
+    # 첫 번째 섬, 비용
     heap = [(0, 0)]
     res = 0
 
     while heap:
-        cost, island = heapq.heappop(heap)
+        cost, island = heappop(heap)
         if visited[island]: continue
         visited[island] = True
         res += cost
         for n_island, n_cost in graph[island]:
             if not visited[n_island]:
-                heapq.heappush(heap, (n_cost, n_island))
+                heappush(heap, (n_cost, n_island))
 
     return res
