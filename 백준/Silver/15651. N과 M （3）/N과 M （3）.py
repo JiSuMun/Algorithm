a04b5dc9b13e1d66e7c10c25c004a86aa21c315d
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-08-03 11:27:46 +0900

    [Silver III] Title: N과 M (3), Time: 1432 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+li = []
+
+def solve():
+    if len(li) == M:
+        print(' '.join(map(str, li)))
+        return
+    for i in range(1, N+1):
+        li.append(i)
+        solve()
+        li.pop()
+solve()
