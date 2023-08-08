23e6f44360fdb6f17a4364366a4762baa0b60075
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-08-09 02:29:44 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+T = int(input())
+for t in range(T):
+    N, M = map(int, input().split())
+    bridge = factorial(M) // (factorial(N) * factorial(M - N))
+    print(bridge)
