9088db42b749470ce46e0fe2f4f295a25749d78d
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-09-02 15:41:49 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 396 ms, Memory: 61,364 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,24 @@
+def is_pal_idx(arr, leng):
+    for lst in arr:
+        for i in range(100-leng+1):
+            for j in range(leng//2):
+                if lst[i+j] != lst[i+leng-1-j]:
+                    break
+            else:
+                return True
+    return False
+
+
+T = 10
+for _ in range(1, T+1):  # 10개 테스트
+    t = int(input())
+    maxNum = 0
+
+    arr1 = [input().strip() for _ in range(100)]
+    arr2 = [''.join(x) for x in zip(*arr1)]
+
+    for leng in range(100, 1, -1):
+        if is_pal_idx(arr1, leng) or is_pal_idx(arr2, leng):
+            break
+
+    print("#{} {}".format(t, leng))
