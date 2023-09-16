d7549dc28267de508fd337950db3125a25f4064d
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-09-16 10:32:57 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,142 ms, Memory: 261,352 KB -BaekjoonHub

---
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -0,0 +1,13 @@
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    nums = list(map(int, input().split()))
+    res = 0
+    price = 0
+    for num in nums[::-1]:
+        if num >= price:
+            price = num
+        else:
+            res += price - num
+
+    print("#{} {}".format(t, res))
