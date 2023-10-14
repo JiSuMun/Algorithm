bcd5f597df6a180ce01a37e489a642b3d0f1c182
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-10-14 15:53:03 +0900

    [D2] Title: 파스칼의 삼각형, Time: 98 ms, Memory: 45,588 KB -BaekjoonHub

---
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,14 @@
+T = int(input())
+for t in range(1, T + 1):
+    res = 0
+    N = int(input())
+    pas = [0] * N
+    for i in range(N):
+        pas[i] = [1] * (i + 1)
+    for j in range(2, N):
+        for k in range(1, len(pas[j])):
+            if k < len(pas[j]) - 1:
+                pas[j][k] = pas[j - 1][k - 1] + pas[j - 1][k]
+    print(f"#{t}")
+    for i in range(N):
+        print(*pas[i])
