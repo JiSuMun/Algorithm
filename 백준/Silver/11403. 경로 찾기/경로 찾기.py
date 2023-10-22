1b1c77d209fabd96a9e840865542095249db9530
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-10-22 23:22:48 +0900

    [Silver I] Title: 경로 찾기, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if graph[j][i] and graph[i][k]:
+                graph[j][k] = 1
+
+for a in graph:
+    print(*a)
