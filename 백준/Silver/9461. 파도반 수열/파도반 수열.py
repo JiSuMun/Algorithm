46269cf3611fcf480945e0a675e5e42259bd0901
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-10-22 23:16:04 +0900

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+P = [0 for _ in range(101)]
+P[1], P[2], P[3] = 1, 1, 1
+for i in range(4, 101):
+    P[i] = P[i - 3] + P[i - 2]
+
+for _ in range(T):
+    print(P[int(input())])
