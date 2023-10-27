d8bae89ce669af7e9e4f7bdbda8e400803bbc145
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-10-27 12:46:23 +0900

    [Gold V] Title: 강의실, Time: 272 ms, Memory: 47828 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -1,26 +1,24 @@
-import heapq
 import sys
+from heapq import heappop, heappush
 
 input = sys.stdin.readline
 
 N = int(input())
-room = [list(map(int, input().split())) for _ in range(N)]
+lectures = []
+for _ in range(N):
+    num, s, e = map(int, input().split())
+    # 강의 번호는 필요없으므로 시작시간과 끝나는 시간만 사용
+    lectures.append((s, e))
 
-# 강의 번호, 강의 시작 시간, 강의 종료 시간 => 시작 시간 기준 정렬
-room.sort(key=lambda x: x[1])
-h = []
-cnt = 0
-for i in room:
-    # h에 값이 존재하고, 가장 일찍 끝나는 시간보다 시작 시간이 늦으면 가장 작은 값(이전 수업 값) pop
-    while h and h[0] <= i[1]:
-        heapq.heappop(h)
+# 강의 시작 시간, 강의 종료 시간 => 시작 시간 기준 정렬
+lectures.sort(key=lambda x: x[0])
+
+heap = []
+for s, e in lectures:
+    # 가장 일찍 끝나는 시간보다 시작 시간이 늦으면 가장 작은 값(이전 수업 값) pop
+    if heap and s >= heap[0]:
+        heappop(heap)
     # 힙에 끝나는 시간 추가 (다음 시작시간과 비교하기 위해)
-    heapq.heappush(h, i[2])
-    cnt = max(cnt, len(h))
+    heappush(heap, e)
 
-print(cnt)
-
-"""
-dfs 시도 => 시간초과
-파이썬 heapq 라이브러리 => 기본적으로 최소힙으로 구현
-"""
+print(len(heap))
