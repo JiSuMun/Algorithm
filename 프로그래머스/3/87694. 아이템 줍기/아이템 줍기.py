4b19175f29c3f02fc0be2db3f11c61485f151678
JiSuMun <118615476+JiSuMun@users.noreply.github.com>
2023-11-28 13:55:31 +0900

    [level 3] Title: 아이템 줍기, Time: 0.33 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -6,7 +6,7 @@ dy = [0, 0, -1, 1]
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
 
-    field = [[-1] * 102 for i in range(102)]
+    board = [[-1] * 101 for i in range(101)]
     
     for r in rectangle:
     	# 모든 좌표값을 2배 해줌
@@ -15,15 +15,15 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
             for j in range(y1, y2+1):
                 # 직사각형 내부이면 0으로 바꿈
                 if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
+                    board[i][j] = 0
                 # 직사각형의 테두리일 때 1로 바꿈
-                elif field[i][j] != 0:
-                    field[i][j] = 1
+                elif board[i][j] != 0:
+                    board[i][j] = 1
                     
     q = deque()
     q.append([characterX * 2, characterY * 2])
     # 아직 방문하지 않은 곳은 1로 채움
-    visited = [[1] * 102 for i in range(102)]
+    visited = [[1] * 101 for i in range(101)]
     # 시작 지점은 0으로 초기화(거리가 0)
     visited[characterX * 2][characterY * 2] = 0
     
@@ -39,7 +39,7 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
             ny = y + dy[k]
             
             # 현재 좌표가 테두리이고 아직 방문하지 않은 곳이라면 q에 추가 후 visited를 최단거리로 갱신
-            if field[nx][ny] == 1 and visited[nx][ny] == 1:
+            if 0<= nx < 101 and 0 <= ny < 101 and board[nx][ny] == 1 and visited[nx][ny] == 1:
                 q.append([nx, ny])
                 visited[nx][ny] = visited[x][y] + 1
     
